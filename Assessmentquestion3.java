344963ebf7f18489e3dbb42b92ca3e672e657b09
Chukka Venkata Ravi <102050741+Chukkaravi@users.noreply.github.com>
2023-01-15 09:28:04 +0530

    Add files via upload

---
+++ Assessmentquestion3.java
@@ -0,0 +1,18 @@
+//Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
+
+public class Assessmentquestion3{
+    public static void main(String[] args){
+        int a = 31;
+        int b = 6;
+        int c = a + b;
+        int d = a - b;
+        int e = a * b;
+        float f = a/b;
+        int g = a%b;
+        System.out.println("Adding of two numbers: "+ c);
+        System.out.println("Subtracting of two numbers: "+ d);
+        System.out.println("Product of two numbers: "+ e);
+        System.out.println("Dividing of two numbers: "+ f);
+        System.out.println("Reminder of two numbers: "+ g);
+    }
+}
